facd2483f199c4ee8034cb507b6c2aa41f5d10e7
ishaanam <ishaana.misra@gmail.com>
2022-03-20 13:07:16 -0400

    add comments and clean up code

--- testnet_bitcoin_wallet/block_logger.py
+++ testnet_bitcoin_wallet/block_logger.py
@@ -1,3 +1,7 @@
+import time
+import signal
+import logging
+
 from ProgrammingBitcoin.bloomfilter import BloomFilter 
 from ProgrammingBitcoin.helper import decode_base58
 from ProgrammingBitcoin.merkleblock import MerkleBlock
@@ -11,28 +15,28 @@ from ProgrammingBitcoin.network import (
 from ProgrammingBitcoin.tx import Tx
 
 from block_utils import * 
-import time
-import signal
-import logging
 
 try:
+    # if possible, import the HOST variable from network_settings.py
     from network_settings import HOST
 except (ModuleNotFoundError, ImportError):
+    # otherwise, create network_settings.py and set the HOST variable to the default node
     with open("network_settings.py", "w") as net_file:
-        net_file.write('HOST = "testnet.programmingbitcoin.com"')
-        HOST = 'testnet.programmingbitcoin.com'
+        HOST = "testnet.programmingbitcoin.com"
+        net_file.write(f"HOST = {HOST}")
 
 logging.basicConfig(filename='block.log', format='%(levelname)s:%(message)s', level=logging.DEBUG)
 
 def block_syncer():
     node = SimpleNode(HOST, testnet=True, logging=False)
     node.handshake()
+    # As long as wallet is running
     while True:
         now_hash = get_latest_block_hash()
         then_hash = read_log(-1)
         current_addr = get_all_addr()
         bf = BloomFilter(size=30, function_count=5, tweak=1729)
-        # when a new block is recieved:
+        # if a new block has been mined 
         if now_hash != then_hash:
             for addr in current_addr:
                 h160 = decode_base58(addr)
@@ -46,7 +50,7 @@ def block_syncer():
 
             for block in headers.blocks:
                 if not block.check_pow():
-                    raise RuntimeError('pow is invalid')
+                   raise RuntimeError('pow is invalid')
                 logging.info(f"recieved block: {block.hash().hex()}")
                 getdata.add_data(FILTERED_BLOCK_DATA_TYPE, block.hash())
                 all_blocks = all_hashes()
@@ -60,6 +64,7 @@ def block_syncer():
             node.send(getdata)
             signal.signal(signal.SIGALRM, handler)
             signal.alarm(40)
+            # for 40 seconds, see if we recieve any relevant transactions
             try:
                 while True:
                     try:
@@ -71,6 +76,7 @@ def block_syncer():
                         else:
                             message.testnet = True
                             ids = get_all_ids()
+                            # check for any transaction outputs pertaining to us
                             for i, tx_out in enumerate(message.tx_outs):
                                 for addr in current_addr:
                                     if tx_out.script_pubkey.address(testnet=True) == addr:
@@ -80,8 +86,10 @@ def block_syncer():
                                         r_user = find_user(addr)
                                         locking_script = tx_out.script_pubkey
                                         block = get_block_hex(merkle_block)
+                                        # set the transaction as confirmed in the corresponding user's file
                                         tx_set_confirmed(r_user, prev_tx, prev_index, prev_amount, addr, locking_script, block)
                                         logging.info(f"{r_user} recieved {prev_amount} satoshis")
+                            # check for any transaction inputs indicating we have spent funds
                             for i, tx_in in enumerate(message.tx_ins):
                                 for tx_id in ids:
                                     if tx_id[0] == tx_in.prev_tx.hex() and int(tx_id[1]) == tx_in.prev_index:
@@ -91,5 +99,6 @@ def block_syncer():
             except RuntimeError:
                 pass
 
+        # wait for ten seconds before checking if more blocks have been mined
         time.sleep(10)
 
