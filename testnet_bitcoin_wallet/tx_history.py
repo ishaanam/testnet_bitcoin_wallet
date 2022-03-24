82093d647807c1b752de65912ab80419a9a1fa8e
ishaanam <ishaana.misra@gmail.com>
2022-03-24 18:16:37 -0400

    display confirmation status in different colors when printing tx history

--- testnet_bitcoin_wallet/tx_history.py
+++ testnet_bitcoin_wallet/tx_history.py
@@ -11,9 +11,9 @@ def get_tx_history(username, show_unconfirmed=False):
     for tx in txs:
         # transaction hash, amount, confirmation status, #of confirmations
         block_hash = tx[-2]
-        confirmations = latest_height - get_height("block_log.csv", block_hash)
+        confirmations = latest_height - get_height("block_log.csv", block_hash) + 1
         if tx[-1] == '0': 
-            status = "belongs to an unconfirmed transaction" 
+            status = "unconfirmed" 
         elif tx[-1] == '1':
             status = "unspent"
         elif tx[-1] == '2' or tx[-1] == '3':
@@ -27,6 +27,10 @@ def get_tx_history(username, show_unconfirmed=False):
     return tx_history
             
 def format_tx_history(username):
+    PURPLE = '\033[94m'
+    RED = '\033[91m'
+    YELLOW = '\033[93m'
+    END = '\033[0m'
     option = input("Would you like to see unconfirmed unspent transaction outputs as well[y/n]: ")
     if option == "y":
         show_unconfirmed = True
@@ -38,7 +42,12 @@ def format_tx_history(username):
         print("TRANSACTION")
         print(f"Transaction ID: {tx[0]}")
         print(f"Amount (Satoshis): {tx[1]}")
-        print(f"Confirmation Status: {tx[2]}")
+        if tx[2] == "unconfirmed":
+            print(f"Confirmation Status: {YELLOW}{tx[2]}{END}")
+        elif tx[2] == "unspent":
+            print(f"Confirmation Status: {PURPLE}{tx[2]}{END}")
+        elif tx[2] == "spent":
+            print(f"Confirmation Status: {RED}{tx[2]}{END}")
         print(f"# of Confirmations: {tx[3]}")
 
     print("\n")
