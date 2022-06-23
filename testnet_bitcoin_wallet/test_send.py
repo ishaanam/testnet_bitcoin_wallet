e429a82a6304d63368048da8a455ce95f0dc587d
ishaanam <ishaana.misra@gmail.com>
2022-06-23 15:37:36 -0400

    add a test for contructing a tx to 2 recipients

--- testnet_bitcoin_wallet/test_send.py
+++ testnet_bitcoin_wallet/test_send.py
@@ -20,8 +20,8 @@ def remove_user(username):
         for user in lines:
             if user[0] == username:
                 lines.remove(user)
-    
-    with open("users.csv", 'w') as users_file: 
+
+    with open("users.csv", 'w') as users_file:
         if lines:
             w = csv.writer(users_file)
             w.writerows(lines)
@@ -87,7 +87,7 @@ class TestWallet(unittest.TestCase):
         new_balance, new_unconfirmed_balance = get_balance(TEST_USERNAME, True)
         assert new_balance == 4000
         assert new_unconfirmed_balance == 2600
-        
+
         # confirm change output from our transaction
         tx_set_confirmed(TEST_USERNAME, tx_obj.id())
 
@@ -100,8 +100,50 @@ class TestWallet(unittest.TestCase):
 
         tx_returns = construct_transaction(recipients, values, TEST_USERNAME)
         tx_obj = tx_returns[0]
+        used_utxos = tx_returns[2]
 
         assert tx_obj.serialize().hex() == target_serialization
+
+        # set utxos bitcoin as "spent"
+        broadcast_transaction(tx_obj, online=False, self_broadcast='y', needs_change=True, username=TEST_USERNAME, used_utxos=used_utxos)
+
+        new_balance, new_unconfirmed_balance = get_balance(TEST_USERNAME, True)
+
+        assert new_balance == 0
+        assert new_unconfirmed_balance == 1200
+
+        # confirm change output from our transaction
+        tx_set_confirmed(TEST_USERNAME, tx_obj.id())
+
+    def test_with_2_recipients(self):
+
+        add_key(TEST_USERNAME, "6a03afdd0e7cbd55d5392d795c83392b6f691a9b8847526322b9e0d2cc67a364","n2JGKD4f9TKdPg62YthhW8Y37C1mT5YSi3")
+
+        add_utxo(TEST_USERNAME, "38db0301a04669b29a57e85e1b3bc73c623073dec7a8f4f666019aa8fa6ca164", 0, 6200, "n2JGKD4f9TKdPg62YthhW8Y37C1mT5YSi3", "OP_DUP OP_HASH160 e3f446b650a7b1558fb6064a8257e40abb5f06a4 OP_EQUALVERIFY OP_CHECKSIG", "0", TXOState.CONFIRMED_UTXO.value)
+
+        recipients = ["msisrAMDRbagHmDH1S92A2QK7vXLWXVEkG", "mu68YHXMEmtcZ1pCLiMv4RLcWbMfWDmEu5"]
+        values = [3000, 3900]
+
+        target_serialization = "0100000002cf74d0bb181e42f6bf90428793e2c15ee461759c6a77513355e4e008d2d269f7010000006a4730440220100c7d2dd207bac5cf838c59bab3fad16f4b589ff814415105bcadeb6ae72f0d02205b7d9006440fc4a1b16e74d1839cee6e2fbf20106912e6bd47f92482921140c401210298d2079527e16a8db18f0be65919825c5cddf63e634f2f7693b0813af1c19e29ffffffff64a16cfaa89a0166f6f4a8c7de7330623cc73b1b5ee8579ab26946a00103db38000000006a4730440220551a22d38f45832067d804ca44c59031bc4ea2e7816674c58f3e6323875371150220125f86d36d35d7ea0c1a13ced74a5fdc99dea7aafb58ea72e38b2dd9a379ceb0012102c9925834169f2484406159fdc9c53e74f5e50620909c1891c74debc8a2cbc1e9ffffffff02b80b0000000000001976a91485e2ca0d69906a9b9d79e491af9c0df0dd482bb588ac3c0f0000000000001976a91494dfea11e40cc312bd821a672eacd73567d3166c88ac00000000"
+
+        tx_returns = construct_transaction(recipients, values, TEST_USERNAME)
+        tx_obj = tx_returns[0]
+        used_utxos = tx_returns[2]
+
+        assert tx_obj.serialize().hex() == target_serialization
+
+
+        assert str(tx_obj.tx_outs[0].script_pubkey) == "OP_DUP OP_HASH160 85e2ca0d69906a9b9d79e491af9c0df0dd482bb5 OP_EQUALVERIFY OP_CHECKSIG" and tx_obj.tx_outs[0].amount == 3000
+        assert str(tx_obj.tx_outs[1].script_pubkey) == "OP_DUP OP_HASH160 94dfea11e40cc312bd821a672eacd73567d3166c OP_EQUALVERIFY OP_CHECKSIG" and tx_obj.tx_outs[1].amount == 3900
+
+
+        # set utxos bitcoin as "spent"
+        broadcast_transaction(tx_obj, online=False, self_broadcast='y', needs_change=False, username=TEST_USERNAME, used_utxos=used_utxos)
+
+        new_balance, new_unconfirmed_balance = get_balance(TEST_USERNAME, True)
+
+        assert new_balance == 0
+        assert new_unconfirmed_balance == 0
 
 if __name__ == '__main__':
     unittest.main()
